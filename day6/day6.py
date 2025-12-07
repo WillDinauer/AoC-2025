9609803f7a3503e06a8623e20f437ec1d9c30a39
WillDinauer <77174175+WillDinauer@users.noreply.github.com>
2025-12-06 22:39:55 -0500

    day6 p2

--- day6/day6.py
+++ day6/day6.py
@@ -1,3 +1,4 @@
+import math
 
 def p1(g):
     res = 0
@@ -13,14 +14,41 @@ def p1(g):
     print(res)
     
 
-def p2(x):
-    pass
+def p2(g):
+    res = 0
+    vals = []
+    for c in range(len(g[0])-1, -1, -1):
+        empty = True
+        for r in range(len(g)-1):
+            if g[r][c] != " ":
+                empty = False
+                break
+        
+        if empty:
+            res += sum(vals) if g[-1][c+1] == "+" else math.prod(vals)
+            vals = []
+            continue
+
+        cur = 0
+        for r in range(len(g)-1):
+            if g[r][c] != " ":
+                cur *= 10
+                cur += int(g[r][c])
+        vals.append(cur)
+    res += sum(vals) if g[-1][0] == "+" else math.prod(vals)
+    print(res)
+
 
 if __name__ == "__main__":
+    filename = "input.txt"
     g = []
-    with open("input.txt", "r") as f:
+    with open(filename, "r") as f:
         line = f.readline().strip().split()
         while line != []:
             g.append(line)
             line = f.readline().strip().split()
     p1(g)
+
+    with open(filename, "r") as f:
+        g = [line.strip("\n") for line in f.readlines()]
+    p2(g)
