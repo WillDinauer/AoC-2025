3b7c3ec910121d3c5d5150b037ef10b0c78fbae7
WillDinauer <77174175+WillDinauer@users.noreply.github.com>
2025-12-16 15:35:00 -0500

    day9 p2 works for sample

--- day9/day9.py
+++ day9/day9.py
@@ -10,40 +10,82 @@ def p1(pts):
                 best = ar
     print(best)
 
-# Number of horizontal lines ABOVE the pt of interest
-def above(pt, horizontal):
-    res = 0
-    # Line: [y, x0, x1]
-    for line in horizontal:
-        if line[0] > pt[1]:
-            return res 
-        if line[1] <= pt[0] <= line[2]:
-            res += 1
-    return res
+# ------ P2 -------
+dirs = {
+    "left": 0,
+    "right": 1,
+    "up": 2,
+    "down" : 3
+}
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+horizontal = []
+vertical = []
+outside = {}
 
 # Check if the pt rests on a line
-def on_line(pt, horizontal, vertical):
+def on_line(pt):
     for line in horizontal:
-        if pt[1] == line[0] and line[1] <= pt[0] <= line[0]:
+        if pt[1] == line[0] and line[1] <= pt[0] <= line[2]:
             return True
     
     for line in vertical:
-        if pt[0] == line[0] and line[1] <= pt[1] <= line[0]:
+        if pt[0] == line[0] and line[1] <= pt[1] <= line[2]:
             return True
+    return False
+
+def vert_check(pt):
+    out = True
+    for line in horizontal:
+        if line[0] > pt[1]:
+            return out
+        
+        # Crossing line
+        if line[1] < pt[1] < line[2]:
+            out = not out
+        
+        # Check corner hit
+        if line[1] == pt[1]:
+            out = dirs["down"] in outside[tuple([line[1], line[0]])]
+
+        if line[2] == pt[1]:
+            out = dirs["down"] in outside[tuple([line[2], line[0]])]
+    return out
 
 # Check if the pt is inside the figure
-def inside(pt, horizontal, vertical):
-    if on_line(pt, horizontal, vertical): 
+def inside(pt):
+    if on_line(pt):
         return True
-    return above(pt, horizontal) % 2 == 1
+    return not vert_check(pt)
 
+def get_dir(a, b):
+    if a[0] == b[0]:
+        return dirs["down"] if a[1] > b[1] else dirs["up"]
+    return dirs["left"] if a[0] > b[0] else dirs["right"]
+
+def get_outside(cur, after):
+    if cur == dirs["down"]:
+        return [] if after == dirs["left"] else [dirs["down"], dirs["left"]]
+    if cur == dirs["up"]:
+        return [] if after == dirs["right"] else [dirs["up"], dirs["right"]]
+    if cur == dirs["right"]:
+        return [] if after == dirs["down"] else [dirs["down"], dirs["right"]]
+    if cur == dirs["left"]:
+        return [] if after == dirs["up"] else [dirs["up"], dirs["left"]]
+    
+    raise RuntimeError(f"Invalid direction provided to get_outside(): {cur}")
 
 def p2(pts):
-    # create horizontal
-    horizontal = []
-    vertical = []
+    # Compute horizontal, vertical, and directions
+
+    direction = get_dir(pts[-1], pts[0])
     for i, pt in enumerate(pts):
-        after = pts[i+1] if i < len(pts)-1 else pts[0] 
+        after = pts[i+1] if i < len(pts)-1 else pts[0]
+        next_dir = get_dir(pt, after)
+
+        outside[tuple(pt)] = get_outside(direction, next_dir)
+        direction = next_dir
 
         # Check for horizontal lines
         if pt[1] == after[1]:
@@ -65,8 +107,9 @@ def p2(pts):
             ar = area(a, b)
 
             # Check if corners are inside fig, then compare
-            if inside(c1, horizontal, vertical) and inside(c2, horizontal, vertical) and ar > best:
-                best = ar
+            if inside(c1) and inside(c2):
+                if ar > best:
+                    best = ar
     print(best)
 
 if __name__ == "__main__":
